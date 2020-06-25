01d2c585516a3614b403c68a69eca3d8061bf309
fahrim27 <fahrim27@gmail.com>
2020-06-25 13:02:23 +0700

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    // protected function authenticated(Request $request, $user)
+    // {
+    //     if(in_array($user->role, [3])){
+    //         return redirect()->url('');
+    //     }
+    //     else{
+    //         return redirect()->url('');
+    //     }
+    // }
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    /**
+     * Log the user out of the application.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+ 
+        $request->session()->flush();
+ 
+        $request->session()->regenerate();
+ 
+        return redirect('masuk')
+            ->withSuccess('Terimakasih, selamat datang kembali!');
+    }
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Auth;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        $user = \Auth::user();
+
+        Gate::define('can_admin', function ($user) {
+            return in_array($user->role, [3]);
+        });
+
+        Gate::define('can_customers', function ($user) {
+            return in_array($user->role, [1]);
+        });
+    }
+}
